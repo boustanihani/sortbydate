e23d9108d7f41776932e4873605722d8bf50d4dd
Hani <boustanihani@gmail.com>
2014-12-23 23:55:06 +0100

    xxx

--- sortbydate.js
+++ sortbydate.js
@@ -1,12 +1,15 @@
 console.log('Starting...\n');
 
 var fs = require("fs");
+var path = require('path');
 var recursive = require('recursive-readdir');
+var moment = require('moment');
+var mkdirp = require('mkdirp');
 
 var myArgs = process.argv.slice(2);
 //console.log(myArgs);
 
-var directory = myArgs[0];
+var directory = path.normalize(myArgs[0]);
 console.log('Ausgewählt: ' + directory + '\n');
 
 fs.stat(directory, function(err, stats) {
@@ -30,6 +33,15 @@ var sort = function() {
 
     console.log('Inhalt wird gelesen...');
 
+    var directoryName = path.basename(directory);
+    var parentDir = path.dirname(directory);
+    console.log('parentDir=' + parentDir + ' && directoryName=' + directoryName);
+
+    var outputDir = path.join(parentDir, directoryName + '-sorted');
+    console.log('outputDir=' + outputDir + '\n\n');
+
+    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
     recursive(directory, function(err, files) {
 
         if (err) return console.log('Es ist ein Fehler aufgetreten!');
@@ -39,7 +51,19 @@ var sort = function() {
 
         files.forEach(function(file, index) {
             console.log(progress(index + 1, length) + file);
-        });
 
+            // http://www.unixtutorial.org/2008/04/atime-ctime-mtime-in-unix-filesystems/
+            var stats = fs.statSync(file);
+            var date = stats.mtime;
+            //console.log(date);
+            var destination = moment(date).format('YYYY/MM/DD');
+            var destinationPath = path.join(outputDir, destination);
+            var fileName = path.basename(file);
+
+            console.log('Moving "' + fileName + '" to ' + destinationPath);
+            if (!fs.existsSync(destinationPath)) mkdirp.sync(destinationPath);
+
+        });
+        console.log('\n\noutputDir=' + outputDir + '\n');
     });
 };
